2480122f8de54e8eb5e570ab8d7ba9422f7b02c6
Durdge-o <45447529+Sushininja05@users.noreply.github.com>
2018-11-28 22:05:29 -0800

    from neocities to github
    my world has begun
    again
    twice

---
+++ fun_zone_area/booshemane.js
@@ -0,0 +1,22 @@
+	function bushmin() 
+		{
+			var fortunelist=[
+			  "Don't eat pringles if some idioet has already taken the cap off.",
+			  "If ya ever get the buttons stuck on a xbox controller, just spit in yer hand and rub it over the stuck buttons. Works everytimeeeeeeee.",
+			  "The kick you do in Super Mario 64 when jumpin is real good for positioning yourself on tight platforms and not dyin'.",
+			  "Blow into a plastic bag, lock it when it is super full of air, hit the bag so hard it pops, piss off whoever is around you.",
+			  "WASD is good, but ESDF gives you 2 extra buttons to work with. If you are ever feeling a bit crampt for buttons then try that, works perfectly after getting used to it.",
+			  "waaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.",
+			  "You gotta shoot the sun to death in order to unlock the secrets.",
+			  "There is a house in LEAGUE OF PISS that is away from the houses that is filled with a SECRET MAN BATTLE, find it if you dare.",
+			  "If you can't find a screw when you are taking something apart, check under the stickers.",
+			  "fuck yeah mariokart fuck yeah mariokart fuck yeah mariokart",
+			  "Krispy, Clean, Creme, Irene, Sleek, Beak, Fleek, Week, MOUNTAIN DEW. solve that riddle--- atheists",
+			  "I CLEAN MY TEETH AND THEY BRIGHT WHITE AND NOW IT SCARES PEOPLE MORE THAN WHEN I HAD BRITISH TEETH WHAT THE FUCK",
+			  "hahahah egg",
+			  "Everyone should strive to be Saxton Hale, a large burly man who wrestles animals for fun and has chest hair in the shape of his country of origin, they also kill everything with bare hands.",
+			  "play team fortress 2 because it is good and ya'll just too stupid to realize it",
+			]; 
+			var fortune=fortunelist[Math.floor(Math.random()*fortunelist.length)]; 
+			document.getElementById("thingo").innerHTML = fortune;
+		}
