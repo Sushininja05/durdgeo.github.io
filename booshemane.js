b4fadeeab67820f85464b945da7c12dc41c12f1e
Durdge-o <45447529+Sushininja05@users.noreply.github.com>
2019-06-27 17:42:31 -0700

    Big Upd8
    Fixes galore, with cheese!

---
+++ booshemane.js
@@ -0,0 +1,37 @@
+	function bushmin() 
+		
+{
+			
+var fortunelist=[
+			  
+"Don't eat pringles if some idioet has already taken the cap off.",
+			  			  
+"The kick you do in Super Mario 64 when jumpin is real good for positioning yourself on tight platforms and not dyin'.",
+			  
+"Blow into a plastic bag, lock it when it is super full of air, hit the bag so hard it pops, piss off whoever is around you.",
+			  
+"waaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.",
+			  
+"You gotta shoot the sun to death in order to unlock the secrets.",
+			  
+"There is a house that is away from the houses that is filled with a SECRET MAN BATTLE, find it if you dare.",
+			  			  
+"fuck yeah mariokart fuck yeah mariokart fuck yeah mariokart",
+			  
+"Krispy, Clean, Creme, Irene, Sleek, Beak, Fleek, Week, MOUNTAIN DEW. solve that riddle--- atheists",
+			  
+"I CLEAN MY TEETH AND NOW IT SCARES PEOPLE EVEN MORE WHAT THE FUCK",
+			  
+"hahahah egg",
+"i'm playing garage in garage with hideo kojima my favorite mascot",	
+"https://gudstuffzinc.us/ heehehoo",	
+"poke through your eYE to touch YOUR bRAIN",
+"JAMP JAMP Jamspg.... SLIDE Sldigosokg LSDIkge",
+"In GRAND you must JUMP over the WALL, very difficult...",
+"GANDBITMITNOLGO",
+]; 
+			
+var fortune=fortunelist[Math.floor(Math.random()*fortunelist.length)]; 
+			
+document.getElementById("thingo").innerHTML = fortune;
+		}
