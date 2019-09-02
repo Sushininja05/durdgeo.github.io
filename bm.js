b1cc15b3f7c391f25bd623f139c918d33d235ec5
Durdge-o <45447529+Sushininja05@users.noreply.github.com>
2019-09-02 05:27:20 -0700

    huge update

---
+++ bm.js
@@ -0,0 +1,71 @@
+	function bushmin() 
+		
+{
+			
+var fortunelist=[
+			  
+"Don't eat pringles if some idioet has already taken the cap off.",
+			  			  
+"The kick you do in Super Mario 64 when jumpin is real good for positioning yourself on tight platforms and not dyin'.",
+			  
+"Blow into a plastic bag, lock it when it is super full of air, hit the bag so hard it pops, piss off whoever is around you.",
+			  
+"waaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.",
+			  
+"You gotta shoot the sun to death in order to unlock the secrets.",
+			  
+"There is a house that is away from the houses that is filled with a SECRET MAN BATTLE, find it if you dare.",
+			  			  
+"fuck yeah mariokart fuck yeah mariokart fuck yeah mariokart",
+			  
+"Krispy, Clean, Creme, Irene, Sleek, Beak, Fleek, Week, MOUNTAIN DEW. solve that riddle--- atheists",
+			  
+"I CLEAN MY TEETH AND NOW IT SCARES PEOPLE EVEN MORE WHAT THE FUCK",
+			  
+"hahahah egg",
+
+"i'm playing garage in garage with hideo kojima my favorite mascot",	
+
+"https://gudstuffzinc.us/ heehehoo",	
+
+"poke through your eYE to touch YOUR bRAIN",
+
+"JAMP JAMP Jamspg.... SLIDE Sldigosokg LSDIkge",
+
+"In GRAND you must JUMP over the WALL, very difficult...",
+
+"GANDBITMITNOLGO",
+
+"Do you know?",
+
+"YOSHI... he's ON THE ROOFF..",
+
+"where is it... my penile machine... ~Infernal",
+
+"you aren't metal unless you've eaten an entire fuckin' bible",
+
+"https://asteroid2k.neocities.org/img/yahoo_gangweed.png",
+
+"I payed $2 for a lock, sold the lock for $5... supply and demand babbyyy",
+
+"I am rich, not in wealth, but in bush.",
+
+"I dislike anime, do not send me it fucker",
+
+"shut up idiot yous smlel",
+
+"add /misc_area/superextradoubletriplefripplesecretpage.html",
+
+"owned",
+
+"Speedos is a gay retard",
+
+"idiot lol",
+
+"play Ultra Fight Da! Kyanta 2!!! it's good!!!",
+]; 
+			
+var fortune=fortunelist[Math.floor(Math.random()*fortunelist.length)]; 
+			
+document.getElementById("thingo").innerHTML = fortune;
+		}
