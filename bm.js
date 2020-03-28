fbff3b033efeceed6b105f6d607db5183f09492b
Durdge-o <45447529+Sushininja05@users.noreply.github.com>
2020-03-27 20:09:48 -0700

    another upd8
    yeehoo

--- bm.js
+++ bm.js
@@ -1,28 +1,22 @@
 	function bushmin() 
-		
+
 {
-			
-var fortunelist=[
-			  
-"Don't eat pringles if some idioet has already taken the cap off.",
-			  			  
+
+var wisdomlist=[
+
 "The kick you do in Super Mario 64 when jumpin is real good for positioning yourself on tight platforms and not dyin'.",
-			  
+
 "Blow into a plastic bag, lock it when it is super full of air, hit the bag so hard it pops, piss off whoever is around you.",
-			  
-"waaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.",
-			  
+
 "You gotta shoot the sun to death in order to unlock the secrets.",
-			  
+
 "There is a house that is away from the houses that is filled with a SECRET MAN BATTLE, find it if you dare.",
-			  			  
+
 "fuck yeah mariokart fuck yeah mariokart fuck yeah mariokart",
-			  
-"Krispy, Clean, Creme, Irene, Sleek, Beak, Fleek, Week, MOUNTAIN DEW. solve that riddle--- atheists",
-			  
+
+"Krispy, Clean, Creme, Irene, Sleek, Beak, Fleek, Week, MOUNTAIN DEW. solve that riddle---",
+
 "I CLEAN MY TEETH AND NOW IT SCARES PEOPLE EVEN MORE WHAT THE FUCK",
-			  
-"hahahah egg",
 
 "i'm playing garage in garage with hideo kojima my favorite mascot",	
 
@@ -34,11 +28,9 @@ var fortunelist=[
 
 "In GRAND you must JUMP over the WALL, very difficult...",
 
-"GANDBITMITNOLGO",
+"Do you know? Did we know?",
 
-"Do you know?",
-
-"YOSHI... he's ON THE ROOFF..",
+"YOSHI... he's ON THE ROOF..",
 
 "where is it... my penile machine... ~Infernal",
 
@@ -50,20 +42,34 @@ var fortunelist=[
 
 "I am rich, not in wealth, but in bush.",
 
-"I dislike anime, do not send me it fucker",
-
-"shut up idiot yous smlel",
+"I dislike it, do not send me it fucker",
 
 "owned",
 
-"Speedos is a gay retard",
-
 "idiot lol",
 
-"play Ultra Fight Da! Kyanta 2!!! it's good!!!",
+"I hold the biggest secrets.",
+
+"BACKFLIP",
+
+"thas'a PRETTY GOOD!!!!",
+
+"H-H-H-H-H-HURRY UP!",
+
+"I once awoke one morning in a sparkling world, in such a dreamy state I couldn't comprehend anything, sadly I had to leave.",
+
+"nice comic-nice comic-- it's name is UNICORN JELLY, give it a READ",
+
+"we shall play smash brother",
+
+"had a dream one morning where I was lost in a neon town, everyone ignored me, I entered an arcade and played DDR, I woke up and missed my bus.",
+
+"one afternoon I fell asleep in my chair, I dreamt of a dull concrete world full of puppet people, I kept pushing past all of them to try and catch some person, I never could.",
+
+"yeah dunno",
 ]; 
 			
-var fortune=fortunelist[Math.floor(Math.random()*fortunelist.length)]; 
+var wisdom=wisdomlist[Math.floor(Math.random()*wisdomlist.length)]; 
 			
-document.getElementById("thingo").innerHTML = fortune;
+document.getElementById("thingo").innerHTML = wisdom;
 		}
