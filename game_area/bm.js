f43f9f17da0e86d408e4fbfb91591b4db4c9b4aa
Durdge-o <nin10dough05@gmail.com>
2022-01-02 21:23:57 -0800

    2022 updat
    yeah

--- game_area/bm.js
+++ game_area/bm.js
@@ -4,61 +4,25 @@
 
 var advicelist=[
 
-"The kick you do in Super Mario 64 when jumpin is real good for positioning yourself on tight platforms and not dyin'.",
-
 "Blow into a plastic bag, lock it when it is super full of air, hit the bag so hard it pops, piss off whoever is around you.",
 
 "You gotta shoot the sun to death in order to unlock the secrets.",
 
-"There is a house that is away from the houses that is filled with a SECRET MAN BATTLE, find it if you dare.",
-
-"fuck yeah mariokart fuck yeah mariokart fuck yeah mariokart",
-
-"Krispy, Clean, Creme, Irene, Sleek, Beak, Fleek, Week, MOUNTAIN DEW. solve that riddle---",
-
-"I CLEAN MY TEETH AND NOW IT SCARES PEOPLE EVEN MORE WHAT THE FUCK",
-
 "i'm playing garage in garage with hideo kojima my favorite mascot",	
-
-"poke through your eYE to touch YOUR bRAIN",
 
 "JAMP JAMP Jamspg.... SLIDE Sldigosokg LSDIkge",
 
-"In GRAND you must JUMP over the WALL, very difficult...",
-
 "Do you know? Did we know?",
-
-"YOSHI... he's ON THE ROOF..",
 
 "where is it... my penile machine... ~Infernal",
 
 "you aren't metal unless you've eaten an entire fuckin' bible",
 
-"https://konservsky.neocities.org/img/yahoo_gangweed.png",
-
-"I payed $2 for a lock, sold the lock for $5... supply and demand babbyyy",
-
 "I am rich, not in wealth, but in bush.",
-
-"I dislike it, do not send me it fucker",
-
-"owned",
-
-"idiot lol",
 
 "I hold the biggest secrets.",
 
-"BACKFLIP",
-
-"thas'a PRETTY GOOD!!!!",
-
 "H-H-H-H-H-HURRY UP!",
-
-"I once awoke one morning in a sparkling world, in such a dreamy state I couldn't comprehend anything, sadly I had to leave.",
-
-"nice comic-nice comic-- it's name is UNICORN JELLY, give it a READ",
-
-"we shall play smash brother",
 
 "had a dream one morning where I was lost in a neon town, everyone ignored me, I entered an arcade and played DDR, I woke up and missed my bus.",
 
@@ -66,21 +30,17 @@ var advicelist=[
 
 "yeah dunno",
 
-"chef--- face revealll??? I HAVE to confrim this.",
+"face revealll??? I HAVE to confrim this.",
 
 "can't even handle a kilogram of GRAPE!",
-
-"large man wear shirt, it's him the shirt man, that's him the SHIRT man",
 
 "shit up, fuck down, idiot",
 
 "I come from Wet Gamin's 2018 Halloween Event, MY EXISTENCE IS RECYCLED.",
 
-"Sometimes, I say funny stuff, other times I talk like this. Weird, huh?",
+"sometimes, I say funny stuff, other times I talk like this. Weird, huh?",
 
-"that gay couple who had their cake denied must have been like 'wow the cake was a lie' winky face colon p",
-
-"not a fan of dopplegangers, that contact guy knows what I'm talking about-- I WAS HERE FIRST... I even lost my spot...",
+"I'm not sure how this whole place got here, it kinda just appeared one day, along with that one miltary guy",
 
 "chinese fan yee haw",
 
@@ -88,11 +48,7 @@ var advicelist=[
 
 "hate when u get a girl toy from mcdoands",
 
-"escaped war in iran and immigrated to america just for my boy to not relish in the pure joys of bush",
-
-"wow, can't even post 2 homies kissing... racist server.",
-
-"couc-h stuck n i need sum1 to come fix it,, will pay u 100",
+"couch stuck n i need sum1 to come fix it,, will pay u 100",
 
 "how two men?",
 
@@ -100,9 +56,9 @@ var advicelist=[
 
 "wasn",
 
-"damn that ass can fart",
+"awhile ago that contact guy bite the dust, it's pretty funny to see that doppleganger finally get one over on em'",
 
-"y'kno losin' my spot on the index has taught me something... hate that contact guy even more",
+"in terms of the lore of this land... I remember it all originating from an era of fire, infinite flames and combusting grounds... probably just a dream",
 ]; 
 			
 var advice=advicelist[Math.floor(Math.random()*advicelist.length)]; 
