f8e64fe929449a58316519c38be9e5f986e5e72b
Durdge-o <nin10dough05@gmail.com>
2022-10-23 21:17:56 -0700

    clean-up
    getting laaaazyyy

---
+++ misc_area/bm.js
@@ -0,0 +1,63 @@
+	function bushmin() 
+{
+var advicelist=[
+
+"I'm a wise shaman, a keeper of the past, a forgotten bush...",
+
+"You gotta shoot the sun to death in order to unlock the secrets.",
+
+"I am rich, not in wealth, but in bush.",
+
+"I hold the biggest secrets.",
+
+"I come from Wet Gamin's 2018 Halloween Event, MY EXISTENCE IS RECYCLED.",
+
+"Not sure why I'm here, really.",
+
+"You think they got bushes at other sites? Nope! Nada!",
+
+"Used to live on the index page some time ago.",
+
+"It's been awhile since I've seen a traveller like you, are you searching for the vault key? Try the new games.",
+
+"The Durdge-o Land is not like it once was, although I'm not sure if that's a bad thing...",
+
+"The world is full of obvious things.",
+
+"It's been awhile since I've seen a traveller like you, are you searching for the vault? Try the art page.",
+
+"I'm not all that useful, what are you standing around for?",
+
+"You've been giving it all you've got, can you give a little more?",
+
+"Be attitude for gains: 1. Be Praying 2. Be Praying 3. Be Praying",
+
+"Everyone has a summer they want to remember.",
+
+"Y'kno it's kind of a miracle that you found me with how hidden I am, thanks for the listen, ya hear?",
+
+"Can't be a has-been if I'm a never-was.",
+
+"You know I find solace being in such a hidden little corner, you learn to appreciate the leaves on your branches.",
+
+"I think people could do with a little more living, most are too scared to enjoy what's there to begin with.",
+
+"Have you heard of Mojibake? It's a term for garbled text that comes through when computers read characters from a different locale. Mainly Japanese to English, I always found it off putting.",
+
+"Have you played Wario Land? It's a great series, but I'd only recommend the 4th one to newcomers.",
+
+"It's pretty hard to find good freeware these days, but if you ever get the chance there's occasionally good stuff on itch.io if you know what to look for. I have <a href='https://itch.io/c/332582/my-faves' style='font-size:9; text-align: center;'>a list</a> if you wanna give any a shot.",
+
+"You should try <a href='https://adriendittrick.itch.io/9-10-do-nightmare' style='font-size:9; text-align: center;'>Nintendo Nightmare</a>, it's insanely rough but you'll love it by the end.",
+
+"Have you ever played <a href='https://www.youtube.com/watch?v=RE7cFOSgIV4' style='font-size:9; text-align: center;'>Umihara Kawase?</a> They're great games.",
+
+"<a href='https://www.youtube.com/watch?v=6YQ6vlEE7NE' style='font-size:9; text-align: center;'>...</a>",
+
+"<a href='../blog_area/blog.html' style='font-size:9; text-align: center;' title='Going back to my stories..'>Oh yeah, you can go if you want. Didn't mean to talk your ear off y'kno!</a>",
+]; 
+			
+var advice=advicelist[Math.floor(Math.random()*advicelist.length)]; 
+			
+document.getElementById("thingo").innerHTML = advice;
+		}
